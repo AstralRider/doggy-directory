c5b4a0d0ce3b9aff50cf8b761831063edf142bd7
AstralRider <100898882+AstralRider@users.noreply.github.com>
2022-09-23 16:24:38 +1000

    testing the search feature

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import mockFetch from "./mocks/mockFetch"
 import App from "./App";
 
@@ -12,7 +13,8 @@ test("renders the landing page",async () => {
   expect(screen.getByRole("heading")).toHaveTextContent(/Doggy Directory/);
   //expect select input to have an exact display value of 'Select a breed'
   expect(screen.getByRole("combobox")).toHaveDisplayValue("Select a breed");
-
+  //verifies the document contains the option 'husky'
+  //-> findBy queries are used when asynchronous code needs to be tested
   expect(await screen.findByRole("option", { name: "husky"})).toBeInTheDocument();
   //expect the search button to be disabled since a selection has not been made
   expect(screen.getByRole("button", { name: "Search" })).toBeDisabled();
@@ -22,7 +24,6 @@ test("renders the landing page",async () => {
 });
 
   //code below to test fetch by setting up and tearing down the mock implementations
-
   //creates a mock function that will track calls to the fetch method attached to the global window variable in the DOM
   beforeEach(()=> {
     //.mockImplementation() accepts a function that will be used to implement the mock method. 
@@ -32,3 +33,34 @@ test("renders the landing page",async () => {
   afterEach(() => {
     jest.restoreAllMocks()
  });
+
+//testing the search feature
+test("should be able to search and display dog image results", async () => {
+   render(<App />);
+   
+   //Simulate selecting an option and verifying its value
+   //getByRole() grabs the selected element and assigns it to the select variable
+   const select = screen.getByRole("combobox");
+   expect(await screen.findByRole("option", { name: "cattledog"})).toBeInTheDocument();
+   userEvent.selectOptions(select, "cattledog");
+   expect(select).toHaveValue("cattledog");
+
+   //Simulate initiating the search request
+   const searchBtn = screen.getByRole("button", { name: "Search" });
+   expect(searchBtn).not.toBeDisabled();
+   userEvent.click(searchBtn);
+
+   //Loading state displays and gets removed once results are displayed
+   await waitForElementToBeRemoved(() => screen.queryByText(/Loading/i));
+
+   //Verify image display and results count 
+   //getAllBy can return more that 1 element
+   const dogImages = screen.getAllByRole("img");
+   //check 2 img's are returned
+   expect(dogImages).toHaveLength(2);
+   expect(screen.getByText(/2 Results/i)).toBeInTheDocument();
+   //verifies appropriate alt text associated with img's
+   expect(dogImages[0]).toHaveAccessibleName("cattledog 1 of 2");
+   expect(dogImages[1]).toHaveAccessibleName("cattledog 2 of 2");
+
+})
